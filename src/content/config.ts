0980ff5e487a7187693f407bf0190c867197f00d
kikekrahe <95240884+kikekrahe@users.noreply.github.com>
2025-10-07 12:56:33 +0200

    Fixing video playback in project slider

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { defineCollection, z } from 'astro:content';
 const imageSchema = z.union([
     z.string(),
     z.object({
-        type: z.string(),
+        type: z.string().transform((val) => val.toLowerCase()).refine((val) => ["image", "video"].includes(val), {message: "type must be 'image' or 'video'"}).optional(),
         src: z.string(),
         caption: z.string().optional(),
     }),
@@ -15,7 +15,7 @@ const projectsCollection = defineCollection({
         title: z.string(),
         categories: z.string().optional(),
         year: z.number(),
-        images: z.array(imageSchema),
+        mediaItems: z.array(imageSchema).optional(),
     }),
 });
 
